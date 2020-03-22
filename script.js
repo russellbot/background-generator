1bcd4a303ba41a59b2a55a54cfb7a07cda426806
Russell Hart <russellbot@gmail.com>
2020-03-22 11:39:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomize = document.querySelector(".randomize");
 
 body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value +")";
 
@@ -13,6 +14,18 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomizeColors() {
+	var randomColor1 = '#' + Math.floor(Math.random()*16777215).toString(16);
+	var randomColor2 = '#' + Math.floor(Math.random()*16777215).toString(16);
+
+	body.style.background = "linear-gradient(to right, " + randomColor1 + ", " + randomColor2 +")";
+
+	css.textContent = body.style.background + ";";
+
+}
+
 color1.addEventListener("input", setGradient)
 
 color2.addEventListener("input", setGradient)
+
+randomize.addEventListener("click", randomizeColors)
