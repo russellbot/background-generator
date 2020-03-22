e2ee6f7f510dcb238829a9766ed49fd24ddc06b9
Russell Hart <russellbot@gmail.com>
2020-03-22 11:10:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value +")";
+
+css.textContent = body.style.background + ";";
+
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value +")";
 
